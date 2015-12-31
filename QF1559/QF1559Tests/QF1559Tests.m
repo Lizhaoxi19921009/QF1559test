ce54951ebf7f809721e0dbf45e3c3b5744b1434a
Lizhaoxi19921009 <547699261@qq.com>
2015-12-31 10:08:05 +0800

    这是我们第一次提交的git

---
+++ QF1559/QF1559Tests/QF1559Tests.m
@@ -0,0 +1,39 @@
+//
+//  QF1559Tests.m
+//  QF1559Tests
+//
+//  Created by MS on 15/12/31.
+//  Copyright © 2015年 MS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QF1559Tests : XCTestCase
+
+@end
+
+@implementation QF1559Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
